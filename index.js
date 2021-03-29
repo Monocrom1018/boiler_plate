5fac8afd58579037fd039fba0226fa9547ca9a51
Monocrom1018 <74134503+Monocrom1018@users.noreply.github.com>
2021-03-30 01:16:21 +0900

    login feature develope half

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port = 5000;
 const mongoose = require("mongoose");
 const { User } = require("./models/User");
 const config = require("./config/key");
+const cookieParser = require("cookie-parser");
+
+app.use(cookieParser());
 
 // application/x-www-form-urlencoded 형태를 분석해줌
 app.use(express.urlencoded({ extended: true }));
@@ -29,11 +32,44 @@ app.post("/register", (req, res) => {
 
   const user = new User(req.body);
 
+  //db에 save하기 전에 bcrypt 를 이용한 비밀번호 암호화
+
   // 몽고디비 메소드. 정보들이 user모델에 저장됨
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
       success: true,
+    });
+  });
+});
+
+app.post("/login", (req, res) => {
+  // 1. 요청된 이메일을 DB에서 탐색
+
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "not exist email",
+      });
+    }
+    // 2. 요청된 이메일이 DB에 있다면, 비밀번호가 맞는 비밀번호인지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "not matched password",
+        });
+      // 3. 비밀번호까지 맞다면, 토큰을 생성
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        // 토큰을 저장한다. 어디에? 쿠키...
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
     });
   });
 });
--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+// salt 가 몇 글자인지 지정. 이걸 이용해서 암호화함
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -10,9 +14,13 @@ const userSchema = mongoose.Schema({
     // 문자열 사이의 스페이스바 공간을 없앰
     trim: true,
   },
+  password: {
+    type: String,
+    minlength: 5,
+  },
   lastname: {
     type: String,
-    maxLength: 50,
+    maxlength: 50,
   },
   role: {
     // 넘버가 0이면 관리자, 1이면 일반유저 이런식으로
@@ -20,6 +28,8 @@ const userSchema = mongoose.Schema({
     default: 0,
   },
   image: String,
+
+  // 봐봐 DB 에 토큰이랑 토큰만료일까지 넣어서 관리함
   token: {
     type: String,
   },
@@ -27,6 +37,51 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+// user 모델에 save하기 전에 콜백함수를 실행하겠다.
+userSchema.pre("save", function (next) {
+  // 비밀번호를 암호화 한다.
+
+  var user = this;
+
+  // 수정된게 password 일 때에만 동작시킨다
+  if (user.isModified("password")) {
+    // 소금 생성
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      // hash -> 암호화된 비밀번호
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+// comparePassword(plainPassword, cb) { ... } 이렇게 갖다쓰면 cb의 두 번째 인자에 true or false 값이 담겨서 { ... } 안에서 쓸 수 있음
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  // painPassword 를 암호화한 다음 db의 password와 비교
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+// generateToken(err, user) { ... } 이렇게 갖다쓰면 user 에 토큰 딱 담겨서 { ... } 안에서 쓸 수 있게 됨
+userSchema.methods.generateToken = function (cb) {
+  var user = this;
+
+  var token = jwt.sign(user._id.toHexString(), "secretToken");
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return db(err);
+    cb(null, user);
+  });
+};
 
 // 위에 짠 스키마를 바탕으로 User 라는 이름의 db모델을 생성
 const User = mongoose.model("User", userSchema);
